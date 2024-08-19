1da6061bd695fc4b48d333bda9c1842734ea5c8c
IbnuJabir <kedirjabir12@gmail.com>
2024-08-19 15:38:53 +0300

    make it executable

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -4,6 +4,7 @@ import asyncio
 
 wait_random = __import__('0-basic_async_syntax').wait_random
 
+
 async def wait_n(n: int, max_delay: int) -> float:
     """Asynchronous coroutine that takes in 2 int arguments (n and max_delay)
     and returns a list of delays"""
