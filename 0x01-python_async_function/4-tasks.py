fa9fd248313e20a6ed1c1b1f6f6550223ed6fc3c
IbnuJabir <kedirjabir12@gmail.com>
2024-08-19 17:36:00 +0300

    alter wait_n function

---
+++ 0x01-python_async_function/4-tasks.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+'''
+this module contains the function for task 4
+'''
+
+import asyncio
+
+task_wait_random = __import__('3-tasks').task_wait_random
+
+
+async def task_wait_n(n: int, max_delay: int) -> asyncio.Task:
+    """
+    Asynchronous coroutine that takes in 2 int arguments (n and max_delay)
+    and returns a list of delays
+    """
+    delays = []
+    for _ in range(n):
+        delays.append(task_wait_random(max_delay))
+    return [await delay for delay in asyncio.as_completed(delays)]
