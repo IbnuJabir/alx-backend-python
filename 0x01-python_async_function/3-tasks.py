04da56a33a8cb7a7937e54ac4e32e9b286f72ea3
IbnuJabir <kedirjabir12@gmail.com>
2024-08-19 17:27:38 +0300

    update the return type of func

--- 0x01-python_async_function/3-tasks.py
+++ 0x01-python_async_function/3-tasks.py
@@ -1,15 +1,21 @@
 #!/usr/bin/env python3
-'''
-    this module contains the function for task 3
-'''
+"""
+This module contains the function for task 3.
+"""
 
 import asyncio
 
 wait_random = __import__('0-basic_async_syntax').wait_random
 
 
-def task_wait_random(max_delay: int) -> float:
+def task_wait_random(max_delay: int) -> asyncio.Task:
     """
-    Returns the total execution time for wait_random(max_delay)
+    Creates and returns an asyncio.Task for the wait_random coroutine.
+
+    Args:
+        max_delay (int): The maximum delay for the wait_random coroutine.
+
+    Returns:
+        asyncio.Task: A Task object that represents the coroutine.
     """
     return asyncio.create_task(wait_random(max_delay))
