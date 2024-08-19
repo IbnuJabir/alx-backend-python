cef7b07133e33e36c6551c2ead6867656f179d26
IbnuJabir <kedirjabir12@gmail.com>
2024-08-19 15:58:44 +0300

    add a documentation for the module

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -1,13 +1,17 @@
 #!/usr/bin/env python3
-
+'''
+this module returns a value after a random delay
+'''
 import asyncio
 
 wait_random = __import__('0-basic_async_syntax').wait_random
 
 
 async def wait_n(n: int, max_delay: int) -> float:
-    """Asynchronous coroutine that takes in 2 int arguments (n and max_delay)
-    and returns a list of delays"""
+    """
+    Asynchronous coroutine that takes in 2 int arguments (n and max_delay)
+    and returns a list of delays
+    """
     delays = []
     for _ in range(n):
         delays.append(wait_random(max_delay))
