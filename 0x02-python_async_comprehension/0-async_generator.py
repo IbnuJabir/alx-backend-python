9481978d6180e8c607255ad4d3b492c23b58a7ac
IbnuJabir <kedirjabir12@gmail.com>
2024-08-20 15:28:43 +0300

    update type annotation

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -1,14 +1,23 @@
 #!/usr/bin/env python3
+'''
+This module contains a solution for task 0
+'''
 
-""" Async Comprehensions """
-
-from asyncio import sleep
-from random import uniform
-from typing import Generator
+import asyncio
+import random
+from typing import AsyncGenerator
 
 
-async def async_generator() -> Generator[float, None, None]:
-    """ Async Generator """
+
+async def async_generator() -> AsyncGenerator[float, None]:
+    """_summary_
+
+    Returns:
+        AsyncGenerator[float, None]: _description_
+
+    Yields:
+        Iterator[AsyncGenerator[float, None]]: _description_
+    """
     for _ in range(10):
-        await sleep(1)
-        yield uniform(0, 10)
+        await asyncio.sleep(1)
+        yield random.uniform(0, 10)
