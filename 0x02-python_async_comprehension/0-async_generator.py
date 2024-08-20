9f4d5598c0f30ab8bfcbb774a4122ab3a0de7a92
IbnuJabir <kedirjabir12@gmail.com>
2024-08-20 14:54:57 +0300

    documentation add for function

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -9,6 +9,14 @@ from typing import AsyncGenerator
 
 
 async def async_generator() -> AsyncGenerator[float, None]:
+    """_summary_
+
+    Returns:
+        AsyncGenerator[float, None]: _description_
+
+    Yields:
+        Iterator[AsyncGenerator[float, None]]: _description_
+    """
     for _ in range(10):
         await asyncio.sleep(1)
         yield random.uniform(0, 10)
