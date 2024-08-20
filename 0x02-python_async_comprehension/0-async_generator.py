2a9b19b9403e7f92e4cc9e38e4d1f0b3642624ac
IbnuJabir <kedirjabir12@gmail.com>
2024-08-20 15:20:42 +0300

    return three values

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -1,23 +1,14 @@
 #!/usr/bin/env python3
-'''
-This module contains a solution for task 0
-'''
 
-import asyncio
-import random
-from typing import AsyncGenerator
+""" Async Comprehensions """
 
+from asyncio import sleep
+from random import uniform
+from typing import Generator
 
 
-async def async_generator() -> AsyncGenerator[float, None, None]:
-    """_summary_
-
-    Returns:
-        AsyncGenerator[float, None]: _description_
-
-    Yields:
-        Iterator[AsyncGenerator[float, None]]: _description_
-    """
+async def async_generator() -> Generator[float, None, None]:
+    """ Async Generator """
     for _ in range(10):
-        await asyncio.sleep(1)
-        yield random.uniform(0, 10)
+        await sleep(1)
+        yield uniform(0, 10)
